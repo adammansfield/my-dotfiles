6f288e29b17b958f1b587c7780bace64249e8b5d
Adam Mansfield <me@adammansfield.com>
2018-12-19 14:41:54 -0500

    Set permissions when creating system links to dotfiles.

--- symlink-dotfiles.sh
+++ symlink-dotfiles.sh
@@ -2,11 +2,19 @@
 
 function link()
 {
-  src_path=$1
-  dst_path=$2
+  source_directory=$1
+  destination_directory=$2
   filename=$3
-  ln -is $src_path/$filename $dst_path/$filename
-  chmod 755 $dst_path/$filename
+  permissions=$4
+
+  destination_file=$destination_directory/$filename
+  if [ -L $destination_file ]; then
+    ln_options=-fs
+  else
+    ln_options=-is
+  fi
+  ln $ln_options $source_directory/$filename $destination_file
+  chmod $permissions $destination_file
 }
 
 script_path="$(cd "$(dirname "$0")" ; pwd -P )"
@@ -14,18 +22,18 @@ script_path="$(cd "$(dirname "$0")" ; pwd -P )"
 mkdir $HOME/.ssh/ &>/dev/null
 mkdir -p $HOME/.terminfo/r &>/dev/null
 
-link $script_path/.ssh $HOME/.ssh config
-link $script_path/.terminfo/r $HOME/.terminfo/r rxvt-unicode-256color
-link $script_path $HOME .bash_aliases
-link $script_path $HOME .bash_functions
-link $script_path $HOME .bash_profile
-link $script_path $HOME .bashrc
-link $script_path $HOME .dockerenv
-link $script_path $HOME .gitconfig
-link $script_path $HOME .gitglobalignore
-link $script_path $HOME .inputrc
-link $script_path $HOME .profile
-link $script_path $HOME .tmux.conf
-link $script_path $HOME .vimrc
-link $script_path $HOME .Xdefaults
-link $script_path $HOME .xmodmap
+link $script_path/.ssh $HOME/.ssh config 600
+link $script_path/.terminfo/r $HOME/.terminfo/r rxvt-unicode-256color 600
+link $script_path $HOME .bash_aliases 755
+link $script_path $HOME .bash_functions 755
+link $script_path $HOME .bash_profile 755
+link $script_path $HOME .bashrc 755
+link $script_path $HOME .dockerenv 755
+link $script_path $HOME .gitconfig 755
+link $script_path $HOME .gitglobalignore 755
+link $script_path $HOME .inputrc 755
+link $script_path $HOME .profile 755
+link $script_path $HOME .tmux.conf 755
+link $script_path $HOME .vimrc 755
+link $script_path $HOME .Xdefaults 755
+link $script_path $HOME .xmodmap 755
