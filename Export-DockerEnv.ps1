c76bd0af48a3f40efaaa12e25b08e4a445e8f124
Adam Mansfield <me@adammansfield.com>
2019-06-03 17:21:59 -0400

    Add -RegenerateCerts parameter to Export-DockerEnv.ps1

--- Export-DockerEnv.ps1
+++ Export-DockerEnv.ps1
@@ -1,38 +1,74 @@
 #.SYNOPSIS
 #  Save the result of 'docker-machine env --shell sh VM_NAME' to easily connect
 #  Windows Subsystem for Linux (WSL) docker to the VM used by Docker for Windows.
-param([string]$VirtualMachine, [string]$OutFile="$PSScriptRoot\.dockerenv")
+param([string]$VirtualMachine="", [string]$OutFile="$PSScriptRoot\.dockerenv", [Switch]$RegenerateCerts=$False)
 
-$windowsIdentitiy = [System.Security.Principal.WindowsIdentity]::GetCurrent();
-$windowsPrincipal = New-Object System.Security.Principal.WindowsPrincipal($windowsIdentitiy);
-$adminRole = [System.Security.Principal.WindowsBuiltInRole]::Administrator;
-if (!$windowsPrincipal.IsInRole($adminRole))
+function Export-DockerEnv()
 {
-    # Launch a new elevated process to re-run this script.
-    $startInfo = New-Object System.Diagnostics.ProcessStartInfo
-    $startInfo.Arguments = "-Command ""& '" + $script:MyInvocation.MyCommand.Path + "'"""
-    $startInfo.FileName = 'PowerShell'
-    $startInfo.Verb = 'runas'
-    $process = [System.Diagnostics.Process]::Start($startInfo)
-    $process.WaitForExit()
+  if (!(IsAdmin))
+  {
+    RelaunchAsAdmin
     exit
+  }
+
+  if (!$VirtualMachine)
+  {
+    Write-Warning "Virtual machine argument is empty; Exporting env of the first virtual machine in the list"
+    $VirtualMachine = GetDefaultVirtualMachine
+  }
+
+  if ($RegenerateCerts)
+  {
+    Write-Host "Regenerating certificates for $VirtualMachine"
+    docker-machine regenerate-certs "$VirtualMachine"
+  }
+
+  $commands = docker-machine env --shell sh $VirtualMachine
+  $commands = $commands -replace "$", "`n"  # Add newlines (without carriage returns).
+	$commands = $commands -replace "C:\\", "/mnt/c/"  # Convert Windows paths to Linux/WSL paths.
+  $commands = $commands -replace "\\", "/"
+  $commands | Out-File -NoNewline -Encoding ASCII $OutFile
+
+  Write-Host "Remember to run symlink-dotfiles.sh in Windows Subsystem For Linux to auto-load .dockerenv"
 }
 
-if (!$VirtualMachine)
+function GetDefaultVirtualMachine()
 {
-  Write-Warning "Virtual machine argument is empty. Exporting env of the first virtual machine in the list."
-  $VirtualMachine = (docker-machine ls)[1].Split(" ")[0]
+  return (docker-machine ls)[1].Split(" ")[0]
 }
 
-$commands = docker-machine env --shell sh $VirtualMachine
+function IsAdmin()
+{
+  $windowsIdentitiy = [System.Security.Principal.WindowsIdentity]::GetCurrent();
+  $windowsPrincipal = New-Object System.Security.Principal.WindowsPrincipal($windowsIdentitiy);
+  $adminRole = [System.Security.Principal.WindowsBuiltInRole]::Administrator;
+  return $windowsPrincipal.IsInRole($adminRole)
+}
 
-# Add newlines (without carriage returns).
-$commands = $commands -replace "$", "`n"
+# Launch a new elevated process to re-run this script.
+function RelaunchAsAdmin()
+{
+  $arguments = "-OutFile $OutFile"
+  if ($VirtualMachine)
+  {
+    $arguments += " -VirtualMachine $VirtualMachine"
+  }
+  if ($RegenerateCerts)
+  {
+    $arguments += " -RegenerateCerts"
+  }
 
-# Convert Windows paths to Linux/WSL paths.
-$commands = $commands -replace "C:\\", "/mnt/c/"
-$commands = $commands -replace "\\", "/"
+  $startInfo = New-Object System.Diagnostics.ProcessStartInfo
+  $startInfo.Arguments = "-Command ""& " + $script:MyInvocation.MyCommand.Path + " $arguments"""
+  $startInfo.FileName = 'PowerShell'
+  $startInfo.Verb = 'runas'
+  $process = [System.Diagnostics.Process]::Start($startInfo)
+  $process.WaitForExit()
+}
 
-$commands | Out-File -NoNewline -Encoding ASCII $OutFile
-
-Write-Host "Remember to run symlink-dotfiles.sh in Windows Subsystem For Linux to auto-load .dockerenv."
+if ($MyInvocation.InvocationName -ne '.')
+{
+  $script:ErrorActionPreference = 'Stop'
+  $script:WarningPreference = 'Continue'
+  exit Export-DockerEnv @PSBoundParameters
+}
