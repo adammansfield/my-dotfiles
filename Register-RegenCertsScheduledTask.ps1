ee89414a7043a3229195b34edba204cda8d931d1
Adam Mansfield <me@adammansfield.com>
2019-06-06 10:10:43 -0400

    Add file comments for Register-RegenCertsScheduledTask.ps1

--- Register-RegenCertsScheduledTask.ps1
+++ Register-RegenCertsScheduledTask.ps1
@@ -1,3 +1,18 @@
+# .SYNOPSIS
+# Create a task to automatically regenerate docker certificates upon login
+#
+# .DESCRIPTION
+# The Docker VM has a different IP everytime that the computer is restarted.
+# So the certificates become invalid and need to be regenerated.
+#
+# This script will create a Task Scheduler task that will regenerate the
+# certificates, and rerun `docker-machine env --shell sh $VM > ~/.dockerenv`.
+# That will update the WSL environment variables for the new IP
+#
+# .NOTES
+# This script can be run from anywhere, but assumes that ~/my-dotfiles exists
+# in the Windows Subsystem for Linux.
+
 $executable = "bash"
 $argument = "--norc --noprofile -c '~/my-dotfiles/exportdockerenv.sh -RegenerateCerts'"
 
