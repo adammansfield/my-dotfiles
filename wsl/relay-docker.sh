b9daf87310f79a28c7a003a023e706fa6c468063
Adam Mansfield <me@adammansfield.com>
2019-07-04 17:06:27 -0400

    Add confirmation to kill previous relay-docker.sh processes

--- wsl/relay-docker.sh
+++ wsl/relay-docker.sh
@@ -4,7 +4,20 @@
 
 if [ -S /var/run/docker.sock ]; then
   echo "ERROR: $(basename "$0") is already running"
-  exit 1
+  read -r -p "Kill [y/N] " response
+  case "$response" in
+    [nN])
+      exit 1
+      ;;
+    *)
+      rm /var/run/docker.sock
+      pkill -f "npiperelay.exe"
+      echo "Rerun relay-docker.sh"
+      # TODO: do not kill self
+      pkill -f "relay-docker.sh"
+      exit 0
+      ;;
+  esac
 fi
 
 exec socat UNIX-LISTEN:/var/run/docker.sock,fork,group=docker,umask=007 EXEC:"npiperelay.exe -ep -s //./pipe/docker_engine",nofork
